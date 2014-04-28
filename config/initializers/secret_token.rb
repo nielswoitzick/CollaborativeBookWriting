9545c1a5e5328f159e372827cf68daa4f8a22516
Niels Woitzick <niels.woitzick@t-online.de>
2014-04-28 11:19:09 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CollaborativeBookWriting::Application.config.secret_token = 'e493c75f24c3ecf1c6a689f196a978a1f5ede07e47ed8a450557c654185c6136d36b5aa2fbad55b10a3f8f035708dd64e1e84b6309fb8af4f809c7c86859aafa'
