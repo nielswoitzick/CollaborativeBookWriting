0f6d053f80d4bad8452ae6ad87a3ea7f638d4473
Niels Woitzick <niels.woitzick@t-online.de>
2014-05-11 19:56:55 +0200

    Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 CollaborativeBookWriting::Application.routes.draw do
-  get "home/index"
+  get 'home/index'
 
   get 'log_in' => 'sessions#new', :as => 'log_in'
   get 'log_out' => 'sessions#destroy', :as => 'log_out'
   get 'sign_up' => 'users#new', :as => 'sign_up'
-  root :to => 'users#new'
+
+  root :to => 'home#index'
+
   resources :users
   resources :sessions
 
