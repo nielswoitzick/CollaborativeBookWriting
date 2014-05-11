5b1af6cfc15e8e0806fc52dc2d0a6a533cd9f956
Niels Woitzick <niels.woitzick@t-online.de>
2014-05-11 19:43:03 +0200

    Added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
   helper_method :current_user
+  before_filter :require_login
+
   I18n.locale = :de
 
   def default_url_options
@@ -11,5 +13,14 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  private
+
+  def require_login
+    unless current_user
+      flash[:error] = 'You must be logged in to access this section'
+      redirect_to log_in_path
+    end
   end
 end
