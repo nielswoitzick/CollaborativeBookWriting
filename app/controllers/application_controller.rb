1df783519bbb815ff7cb929894772fef576097ef
Niels Woitzick <niels.woitzick@t-online.de>
2014-05-11 19:15:23 +0200

    Added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_user
+  I18n.locale = :de
+
+  def default_url_options
+    {:locale => I18n.locale}
+  end
+
+  private
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
